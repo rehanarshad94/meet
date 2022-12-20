e8839d6ffc5f84f5dba4fce73d54f8579ec6d802
Rehan Arshad <rehanarshad94@yahoo.com>
2022-12-20 15:59:37 -0600

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -91,6 +91,9 @@ module.exports.getAccessToken = async (event) => {
         // Respond with OAuth token 
         return {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
           body: JSON.stringify(token),
         };
       })
@@ -99,6 +102,9 @@ module.exports.getAccessToken = async (event) => {
         console.error(err);
         return {
           statusCode: 500,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
           body: JSON.stringify(err),
         };
       });

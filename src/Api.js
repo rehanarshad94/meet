a3da09a6d90d7caa9f0930ac5b6c28d0711834e1
Rehan Arshad <rehanarshad94@yahoo.com>
2023-01-24 17:50:45 -0600

    update

--- src/Api.js
+++ src/Api.js
@@ -24,6 +24,12 @@ const checkToken = async (accessToken) => {
       NProgress.done();
       return mockData;
     }
+
+    if (!navigator.onLine) {
+      const data = localStorage.getItem("lastEvents");
+      NProgress.done();
+      return data?JSON.parse(events).events:[];;
+    }
   
   
     const token = await getAccessToken();
